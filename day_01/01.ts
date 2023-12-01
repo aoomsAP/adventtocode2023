3db8a5028f37600ba3d66c11500d4763b834b089
Alki Ooms <alki.ooms@student.ap.be>
2023-12-01 19:31:28 +0100

    day 1a

--- day_01/01.ts
+++ day_01/01.ts
@@ -1,3 +1,30 @@
+import fs from "fs";
 
+let input = fs.readFileSync("input.txt", "utf8");
+
+// sample
+// input = `1abc2
+// pqr3stu8vwx
+// a1b2c3d4e5f
+// treb7uchet`
+
+const values : string[] = input.split(`\n`);
+
+// filtering for only digits
+const regex = new RegExp(/\d/)
+
+const digits : number[] = values.map(value => {
+    // keep only digits
+    const onlyDigits = value.split("").reduce((str,char) => {
+        return (regex.test(char)) ? str+char : str;
+    },"");
+
+    // keep only first and last digit
+    return parseInt(onlyDigits[0]+onlyDigits[onlyDigits.length-1]);
+});
+
+console.log("What is the sum of all of the calibration values?");
+const sumOfValues = digits.reduce((sum,value) => sum+value);
+console.log(sumOfValues);
 
 export { }
