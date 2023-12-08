5ced84a98bb4b58e10a4911ac6f1045655fc8faf
Alki Ooms <alki.ooms@student.ap.be>
2023-12-08 06:52:17 +0100

    day 8a

--- day_08/a.ts
+++ day_08/a.ts
@@ -3,9 +3,73 @@ import fs from "fs";
 let input = fs.readFileSync("input.txt", "utf8");
 
 // sample
-// input = `samplecode`
+// input = `RL
 
+// AAA = (BBB, CCC)
+// BBB = (DDD, EEE)
+// CCC = (ZZZ, GGG)
+// DDD = (DDD, DDD)
+// EEE = (EEE, EEE)
+// GGG = (GGG, GGG)
+// ZZZ = (ZZZ, ZZZ)`
 
-// console.log(test);
+// extra sample
+// input = `LLR
+
+// AAA = (BBB, BBB)
+// BBB = (AAA, ZZZ)
+// ZZZ = (ZZZ, ZZZ)`
+
+const directions = input.split("\n")[0];
+
+const network = input.split("\n\n")[1].split("\n");
+
+interface Node {
+    value: string,
+    left: string,
+    right: string,
+}
+
+const parseNode = (node: string) => {
+    let rLeft = node.match(/\(([^,]+),/) as RegExpMatchArray;
+    let rRight = node.match(/,\s([^\)]+)\)/) as RegExpMatchArray;
+    return {
+        value: node.substring(0, 3),
+        left: rLeft[1],
+        right: rRight[1],
+    }
+}
+
+const findNode = (nodes: Node[], target: string) => {
+    return nodes.find(n => n.value === target);
+}
+
+const nodes = network.map(n => parseNode(n));
+
+let zzzReached = false;
+let steps = 0;
+let directionIndex = 0;
+let currentNode = findNode(nodes,"AAA");
+
+while (!zzzReached) {
+    steps++;
+    let currentDirection = directions[directionIndex];
+    let nextNode : Node|undefined = undefined;
+
+    if (currentDirection === "L") {
+        nextNode = findNode(nodes,currentNode?.left as string);
+    } else {
+        nextNode = findNode(nodes,currentNode?.right as string);
+    }
+
+    if (nextNode?.value === "ZZZ") {
+        zzzReached = true;
+    }
+
+    directionIndex = (directionIndex === directions.length-1) ? 0 : directionIndex+1;
+    currentNode = nextNode;
+}
+
+console.log(steps);
 
 export { }
